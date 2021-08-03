6fdbab7e711b5158cce4d1d0c47b5e5137276466
julian mercado <gustavo.n.mercado2@gmail.com>
2021-08-03 20:29:43 -0300

    config set to production

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,14 +2,14 @@ import dotenv from 'dotenv';
 const envParsed = dotenv.config().parsed || {};
 
 const config = {
-  //You need to create a db name manually in Mysql and put the name of database in .env file
+  //You need to create a db name manually in Mysql
   dbName: "RecipeChallenge",
   port: envParsed.PORT || 3000,
   /*AuthToken is to set authorization, if it is true, it will block queries some if you haven't an user
     bearer token in your header so you maybe have to use an aplication like Postman or Insomnia,
     you can set it to false to query in a more comfortable way in your browser
     */
-  authToken: false,
+  authToken: true,
   encryptPassword: envParsed.JWT_ENCRYPT_PASSWORD || '',
   graphqlPath: '/graphql',
 };
